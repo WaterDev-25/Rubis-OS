08e814f2a43d93fdf8eaadfca104e98285dec6f0
WaterDev <67019702+WaterDev-25@users.noreply.github.com>
2021-09-10 20:50:55 +0200

    just add empty_end.asm

--- compile.bat
+++ compile.bat
@@ -3,6 +3,9 @@ cd src
 :: Compile first stage of boot loader
 nasm boot/boot.asm -f bin -o ../build/boot.bin
 
+:: Compile empty_end.asm
+nasm kernel/empty_end.asm -f bin -o ../build/empty_end.bin
+
 :: Compile second stage of boot loader
 nasm boot/kernel_entry.asm -f elf32 -o ../build/kernel_entry.o
 wsl $WSLENV/x86_64-elf-gcc -O0 -g -ffreestanding -m32 -c kernel/kernel.cpp -o ../build/kernel.o
@@ -11,6 +14,7 @@ wsl $WSLENV/x86_64-elf-ld -o loader.bin -m elf_i386 -Ttext 0x1000 kernel_entry.o
 
 :: Make terminal image
 wsl cat boot.bin loader.bin > bootloader.bin
+wsl cat bootloader.bin empty_end.bin > os.bin
 
 cd ..
 
