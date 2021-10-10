ee400e9d1fc08e3f1531854d423b5aeb4f7d0012
WaterDev-25 <jpradiolibre@gmail.com5>
2021-10-10 20:19:36 +0200

    abs function

--- src/kernel/maths/math.cpp
+++ src/kernel/maths/math.cpp
@@ -3,3 +3,10 @@ double sqrt(double x){ //problem?
     asm("fsqrt" : "=t" (res) : "0" (x));
     return res;
 }
+
+int abs(int n){
+    if(n>=0)
+        return n;
+    else
+        return -1*n;
+}
--- src/kernel/maths/math.h
+++ src/kernel/maths/math.h
@@ -1,4 +1,6 @@
 #ifndef MATHS_H
 #define MATHS_H
 double sqrt (double x);
+int rand(void);
+int abs(int n);
 #endif
